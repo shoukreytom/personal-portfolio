bd1480ff2724e449f27c10ae1079ec947a6f0da3
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2020-12-28 13:01:23 +0200

    removed about, resume, portfolio, and contact views and urls

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('resume/', views.resume, name='resume'),
-    path('portfolio/', views.portfolio, name='portfolio'),
     path('portfolio/<int:pk>/', views.portfolio_details, name='portfolio-details'),
-    path('contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,17 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home/index.html")
 
-def about(request):
-    return render(request, "home/about.html")
-
-def resume(request):
-    return render(request, "home/resume.html")
-
-def portfolio(request):
-    return render(request, "home/portfolio.html")
-
-def contact(request):
-    return render(request, "home/contact.html")
-
 def portfolio_details(request, pk):
     return render(request, "home/portfolio-details.html")
