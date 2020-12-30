6618fc61d0ce7d6669e226cf1ad7d65887eef4b5
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2020-12-30 15:36:10 +0200

    added send_email view and send-mail router

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('portfolio/<int:pk>/', views.portfolio_details, name='portfolio-details'),
+    path('feedback/send_mail/', views.send_email, name='send-email'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseBadRequest, HttpResponse
+from django.core.mail import send_mail
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.urls import reverse
+from django.contrib import messages
 
 
 def home(request):
@@ -6,6 +12,20 @@ def home(request):
 
 def portfolio_details(request, pk):
     return render(request, "home/portfolio-details.html")
+
+@csrf_exempt
+def send_email(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        message = name + "\n" + email + "\n" + message
+        send_mail(subject, message, email, [settings.EMAIL_HOST_USER])
+        messages.success(request, "The message has been sent successfully")
+        return redirect(reverse('home'))
+    else:
+        return HttpResponse("<h1>bad request</h1>")
 
 
 def error404(request, exception=None):
