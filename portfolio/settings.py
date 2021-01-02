b3fdd8a730cd3e71e3c549300266e14f1953b334
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2021-01-02 22:56:17 +0200

    added MEDIA_URL and MEDIA_ROOT vars

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
