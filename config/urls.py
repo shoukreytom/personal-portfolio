e3ae188bf64ae6253dbe2c3b0b40edaf15b457b8
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2021-02-14 22:05:22 +0200

    server static and media files when DEBUG is True

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^summernote/', include('django_summernote.urls')),
 ]
 
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
